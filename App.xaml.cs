e79cf6f0be8e7e75b67d2a7a25778617af79ddf6
Jan Palme <st64163@upce.cz>
2022-10-16 05:47:20 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BTEJA_BCSH2_semestralka
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
