5961697fa6d5186c989657c42c840fd7b9d79e92
ST64163 <st64163@upce.cz>
2022-12-27 01:09:59 +0100

    cosmetics

--- BCSH2_semestralka/MainWindow.xaml.cs
+++ BCSH2_semestralka/MainWindow.xaml.cs
@@ -1,6 +1,4 @@
-﻿
-using System;
-using System.Windows;
+﻿using System.Windows;
 
 namespace BTEJA_BCSH2_semestralka;
 
@@ -22,11 +20,20 @@ public partial class MainWindow : Window
     {
         textBox_consoleInput.IsEnabled = isEnabled;
         button_insertInput.IsEnabled = isEnabled;
+        if (isEnabled)
+            textBox_consoleInput.Focus();
     }
 
     public void SetInterpretState(bool isEnabled)
     { 
         button_build.IsEnabled = isEnabled;
         button_interpret.IsEnabled = isEnabled;
+        if (isEnabled)
+            textBox_editor.Focus();
+    }
+
+    private void Click_SwitchShowLogs(object sender, RoutedEventArgs e)
+    {
+        checkBox_showLogs.IsChecked = !checkBox_showLogs.IsChecked;
     }
 }
